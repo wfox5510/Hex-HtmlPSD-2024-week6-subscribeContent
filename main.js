d1be60e09a1d2584cf80a4c6b3c5435d9b44a9ac
wfox5510 <wfox5510@gmail.com>
2024-08-19 00:59:52 +0800

    complete theme layout/working on swiper

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+var swiper = new Swiper(".mySwiper", {
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+  renderFraction: function (currentClass, totalClass) {
+    return '<span class="' + currentClass + '"></span>' +
+      ' of ' +
+      '<span class="' + totalClass + '"></span>';
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
