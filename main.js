39e0775f5a2fa43f6e54039c69facbc2ec918990
wfox5510 <wfox5510@gmail.com>
2024-08-22 01:12:30 +0800

    complete life and hot layout

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 // 參考 https://codepen.io/lpla/pen/wvqMvML 做出兩個斷點的paganation
-var DoublePaganation = new Swiper(".mySwiper", {
+var DoublePaganation = new Swiper(".article-swiper", {
+  spaceBetween: 48, 
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
   pagination: {
     el: '.swiper-pagination2',
     type: "fraction",
   },
 })
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".article-swiper", {
   spaceBetween: 48, 
   breakpoints: {
     992: {
@@ -27,3 +34,19 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 swiper.controller.control = DoublePaganation;
+
+var hotsSwiper = new Swiper(".hots-swiper", {
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+const projectSwiper = new Swiper(".project-swiper", {
+  slidesPerView: 2,
+  grid: {
+    fill: 'column',
+    rows: 2,
+  },
+});
